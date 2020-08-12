67b2098286640626d94c8cfd27bb42cfb5c63f8e
kadenswarr <kaden.swarr97@gmail.com>
2020-08-12 05:25:08 -0600

    Updates

--- daily_weather_grabber.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-cmd /k "C:\Users\Kaden\a_weather_project\weathervenv\Scripts\activate & cd /d	C:\Users\Kaden\a_weather_project\weatherdata_grabber & scrapy crawl daily -o weather_data.csv"
